4e6ea6b42c20cb4fe41fe15532c2d10d3bc9010c
plikipe <plikipie@gmail.com>
2020-11-02 02:00:24 +0700

    added responsive layout

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,9 @@
 import "regenerator-runtime";
+import "../styles/appbar.css";
+import "../styles/styles.css";
+import "../styles/main.css";
+import "../styles/content.css";
+import "../styles/footer.css";
+import "../styles/responsive.css";
 
 console.log("Hello coders!");
