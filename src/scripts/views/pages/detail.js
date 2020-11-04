6712e842dbe4a71544d480c31bd30055e07d7274
plikipe <plikipie@gmail.com>
2020-11-04 15:46:01 +0700

    make a simple URL

---
+++ src/scripts/views/pages/detail.js
@@ -0,0 +1,12 @@
+const Detail = {
+  async render() {
+    return `
+    <h2>Detail Page</h2>
+    `;
+  },
+  async afterRender() {
+    // fungsi ini akan dipanggil setelah render()
+  },
+};
+
+export default Detail;
